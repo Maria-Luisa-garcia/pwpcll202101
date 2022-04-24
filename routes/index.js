bcb028658e414faeff012ad3b9ae0be74655beea
maria-luisa-garcia <garciatellezmarialuisa@gmail.com>
2022-04-24 14:15:37 -0500

    Agregando nombre de compañia.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: 'Garcia Tellez Maria Luisa', appName: 'WebApp' });
+  res.render('index', { title: 'Express', author: 'Garcia Tellez Maria Luisa', appName:
+   'WebApp', company:'Awsome Software' });
 });
 
 /*Agregando nueva ruta*/
